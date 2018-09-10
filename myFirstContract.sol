ef92194f3b11b4892ee1a08326ffe65e55f6983e
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-10 22:38:33 +0530

    Extending Inheritance

--- myFirstContract.sol
+++ myFirstContract.sol
@@ -1,6 +1,47 @@
 pragma solidity ^0.4.0;
 
-contract MyFirstContract {
+
+interface Regulator {
+    function checkValue(uint amount) returns (bool);
+     function loan() returns (bool);    
+}
+
+contract Bank is Regulator {
+    
+    uint private value; 
+    
+    function Bank(uint amount){
+        value = amount ; 
+    }
+    
+    function deposit(uint amount){
+        value += amount;
+    }
+    
+    function withdraw(uint amount){
+        if (checkValue(amount)){
+        value -= amount;}
+    }
+    
+    function balance() returns (uint){
+        return value;
+    }
+    
+      function loan() returns (bool){
+        
+        return value > 0 ;
+    }
+     function checkValue(uint amount) returns (bool){
+        
+        return amount >= value;
+    }
+    
+}
+
+// extends == is 
+
+contract MyFirstContract is Bank(10) {
+    
     string private name; 
     uint private age;
     
@@ -19,6 +60,7 @@ contract MyFirstContract {
     function getAge() returns (uint) {
         return age;
     }
+    
 }
 
 // https://remix.ethereum.org/#optimize=false&version=soljson-v0.4.24+commit.e67f0147.js
