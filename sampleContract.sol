8c1b62b14bb5c5aa000d037aaa664d0ebdd9c778
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-13 13:47:14 +0530

    Sample Contract

---
+++ sampleContract.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.23; 
+
+
+// Create Getters and Setters 
+
+contract smartContract {
+    
+    struct Instructor{
+        bytes name;
+        uint age;
+        bytes subject; 
+        
+    }
+    
+    mapping(address => Instructor) instructors; 
+    address[] public instructorAccts;
+    
+    event instructorInfo(
+        
+        bytes name,
+        uint age,
+        bytes subject
+  
+        );
+    
+    function setInstructor(address _address, uint _age, bytes _subject , bytes _name ) {
+        
+        var instructor = instructors[_address];
+        
+        instructor.age = _age;
+        instructor.subject = _subject;
+        instructor.name = _name;
+    }
+    
+    function getInstructor() view public returns (address[]){
+        return instructorAccts;
+    }
+
+    function getInstructor(address _address) view public returns (uint , bytes, bytes){
+        return (instructors[_address].age , instructors[_address].name , instructors[_address].subject);
+    }
+    
+    function countInstructors() view public returns (uint){
+        return instructorAccts.length;
+    }
+    
+}
